d1167c1d46a4390cc4153bdbe366255312a3b508
Цема Егор <pygame51@gmail.com>
2022-01-12 17:49:33 +0200

    fill data in the database

--- djangogramm/settings.py
+++ djangogramm/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
 
 STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
